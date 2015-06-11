456f9f080e03dda854bb882265a134add115e380
guuilp <guuuilp@gmail.com>
2015-06-11 00:16:41 -0300

    Ajuste do nome dos arquivos de 'produto' para 'livro'

--- js/app.js
+++ js/app.js
@@ -2,15 +2,15 @@ var app = angular.module('app', ['ngRoute']);
 
 app.config(['$routeProvider',function($routeProvider) {
 	$routeProvider
-	.when('/cadastrar_produto', {
-		title: 'Cadastrar Produto',
-		templateUrl: 'views/cadastrar_produto.html',
-		controller: 'cadastrar_produto_controller'
+	.when('/cadastrar_livro', {
+		title: 'Cadastrar Livro',
+		templateUrl: 'views/cadastrar_livro.html',
+		controller: 'cadastrar_livro_controller'
 	})
-	.when('/consultar_produto', {
-		title: 'Consultar Produto',
-		templateUrl: 'views/consultar_produto.html',
-		controller: 'consultar_produto_controller'
+	.when('/consultar_livro', {
+		title: 'Consultar Livro',
+		templateUrl: 'views/consultar_livro.html',
+		controller: 'consultar_livro_controller'
 	})
 	.when('/cadastrar_cliente',{
 		title: 'Cadastrar Cliente',
