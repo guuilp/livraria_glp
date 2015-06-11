3ae3178439e6f19d7b453aac7e8ac668246e7b4d
guuilp <guuuilp@gmail.com>
2015-06-11 00:13:26 -0300

    Removendo comentários

--- cadastrar_cliente.php
+++ cadastrar_cliente.php
@@ -1,34 +1,26 @@
 <?php
-   $dbh = new PDO('pgsql:host=localhost;dbname=livraria_glp', 'postgres');
-   /*
-   * Collect all Details from Angular HTTP Request.
-   */ 
-    $postdata = file_get_contents("php://input");
-    $request = json_decode($postdata);
-    @$nome        = $request->nome;
-    @$cpf         = $request->cpf;
-    @$rg          = $request->rg;
-    @$nascimento  = $request->nascimento;
-    @$email       = $request->email;
-    @$telefone    = $request->telefone;
-    @$cep         = $request->cep;
-    @$endereco    = $request->endereco;
-    @$numero      = $request->numero;
-    @$complemento = $request->complemento;
-    @$bairro      = $request->bairro;
-    @$cidade      = $request->cidade;
-    @$estado      = $request->estado;
-    @$pais        = $request->pais;
+  $dbh = new PDO('pgsql:host=localhost;dbname=livraria_glp', 'postgres');
+  /*
+  * Recuperando todos os detalhes da requisição HTTP do Angular
+  */ 
+  $postdata = file_get_contents("php://input");
+  $request = json_decode($postdata);
+  @$nome        = $request->nome;
+  @$cpf         = $request->cpf;
+  @$rg          = $request->rg;
+  @$nascimento  = $request->nascimento;
+  @$email       = $request->email;
+  @$telefone    = $request->telefone;
+  @$cep         = $request->cep;
+  @$endereco    = $request->endereco;
+  @$numero      = $request->numero;
+  @$complemento = $request->complemento;
+  @$bairro      = $request->bairro;
+  @$cidade      = $request->cidade;
+  @$estado      = $request->estado;
+  @$pais        = $request->pais;
 
-    //print_r($request);
-
-     echo $nome;
-
-    /*
-     * You can use $email and $pass for further work. Such as Database calls.
-    */
-
-  $dbh->exec("INSERT INTO CLIENTE VALUES( DEFAULT      , 
+  $dbh->exec("INSERT INTO CLIENTE VALUES( DEFAULT        , 
                                           '$nome'        , 
                                           '$cpf'         , 
                                           '$rg'          , 
@@ -42,6 +34,6 @@
                                           '$bairro'      , 
                                           '$cidade'      , 
                                           '$estado'      , 
-                                          '$pais'       )" ) or die($dbh->errorInfo());
+                                          '$pais'       )" ) or die( $dbh->errorInfo() );
 
 ?>
