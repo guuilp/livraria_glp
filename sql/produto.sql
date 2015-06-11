e76d50cb9c7470363a4381a3226ef44d584ef99a
guuilp <guuuilp@gmail.com>
2015-06-11 00:17:25 -0300

    Inclusão do SQL de criação  da tabela de produto

---
+++ sql/produto.sql
@@ -0,0 +1,13 @@
+CREATE TABLE PRODUTO(
+	id           serial,
+	titulo       varchar(50),
+	subtitulo    varchar(50),
+	isbn         varchar(50),
+	paginas      varchar(50),
+	edicao       varchar(50),
+	editora      varchar(50),
+	ano          varchar(50),
+	assunto      varchar(50),
+	idioma       varchar(50),
+	codigoBarras varchar(50)
+	);
