e76cc3140a1dd81f3202e800dd49a5fa4ceedce5
Guilherme Lima Pereira <guuuilp@gmail.com>
2015-06-24 22:33:56 -0300

    Atualizando cadastrar cliente controller
    Ajustando identação, incluindo bootstrap alert e função de limpar os
    campos

--- controllers/cadastrar_cliente_controller.js
+++ controllers/cadastrar_cliente_controller.js
@@ -18,12 +18,12 @@ app.controller('cadastrar_cliente_controller', function($scope, $http){
     $scope.buscarCep = function(){
     	$http.get('http://viacep.com.br/ws/' + $scope.cliente.cep + '/json/ ')
     		.success(function(data){
-    			$scope.cliente.bairro = data.bairro;
+    			$scope.cliente.bairro      = data.bairro;
     			$scope.cliente.complemento = data.complemento;
-    			$scope.cliente.cidade = data.localidade;
-    			$scope.cliente.endereco = data.logradouro;
-    			$scope.cliente.estado = data.uf;
-    			$scope.cliente.pais = 'Brasil';
+    			$scope.cliente.cidade      = data.localidade;
+    			$scope.cliente.endereco    = data.logradouro;
+    			$scope.cliente.estado      = data.uf;
+    			$scope.cliente.pais        = 'Brasil';
     		})
     		.error(function(){
     			console.error('Não foi possível recuperar o endereço para o CEP digitado.')
@@ -50,10 +50,41 @@ app.controller('cadastrar_cliente_controller', function($scope, $http){
 
 		$http.post('cadastrar_cliente.php', novoCliente)
 			.success(function(){
+                $scope.addAlert('success', 'Cliente inserido com sucesso!');
+                limparCampos();
 				console.log('Cliente inserido com sucesso.');
 			})
 			.error(function(){
-				console.error('Erro ao incluir um novo  cliente');
+                $scope.addAlert('danger', 'Erro ao incluir um novo  cliente!');
+				console.error('Erro ao incluir um novo  cliente.');
 		})
     };
+
+    $scope.alerts = [
+    ];
+
+    $scope.addAlert = function(type, msg) {
+        $scope.alerts.push({type, msg});
+    };
+
+    $scope.closeAlert = function(index) {
+        $scope.alerts.splice(index, 1);
+    };
+
+    var limparCampos = function() {
+        $scope.cliente.nome        = '';
+        $scope.cliente.cpf         = '';
+        $scope.cliente.rg          = '';
+        $scope.cliente.nascimento  = '';
+        $scope.cliente.email       = '';
+        $scope.cliente.telefone    = '';
+        $scope.cliente.cep         = '';
+        $scope.cliente.endereco    = '';
+        $scope.cliente.numero      = '';
+        $scope.cliente.complemento = '';
+        $scope.cliente.bairro      = '';
+        $scope.cliente.cidade      = '';
+        $scope.cliente.estado      = '';
+        $scope.cliente.pais        = '';
+    }
 });
